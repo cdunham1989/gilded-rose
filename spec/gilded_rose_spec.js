e8d59ceaa39f0ce00bce208878546946c994550e
Chris <cdunham1989@gmail.com>
2017-11-28 12:42:41 +0000

    changed first test to ensure items are correctly added to the store, added in a se cond test which ensures quality and sellIn values for standard objects works as expected, both unit tests passing

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,21 @@
 describe("Gilded Rose", function() {
+  var shop;
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("foo");
+  beforeEach(function() {
+    gildedRose = new Shop([ new Item("foo", 10, 10) ]);
   });
 
+  describe("Creating Items", function() {
+    it("creates an item when handed the correct parameters", function() {
+      expect(gildedRose.items[0]).toEqual(jasmine.objectContaining({ name: "foo", sellIn: 10, quality: 10 }));
+    });
+  });
+
+  describe("Updating Item values", function() {
+    it("changes the sellIn and quality values on a normal item after a day has passed", function() {
+      var items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(9);
+      expect(items[0].quality).toEqual(9);
+    });
+  });
 });
